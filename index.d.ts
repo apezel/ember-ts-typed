2695d00fa2308f11c4789fc72db6a98093922eac
Arnaud PEZEL <arnaud@apyx.fr>
2016-10-12 16:42:01 +0200

    initial

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export declare function EmberChain(constructor: Function): any;
+export declare function on(...events: string[]): (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => any;
+export declare function observes(...props: string[]): (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => any;
+export declare function computed(...props: string[]): (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => any;
